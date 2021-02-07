f15ed62bafe316e3495abc2fff103bbc1cca1693
Valentin Alekhin <headbangernetwork@gmail.com>
2021-02-07 14:37:57 +0300

    Add socket in chat

--- client/src/pages/Chat/Chat.jsx
+++ client/src/pages/Chat/Chat.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import queryString from 'query-string'
+import io from 'socket.io-client'
 
 import './Chat.css'
 
-const Chat = () => {
+let socket
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState('')
+  const [room, setRoom] = useState('')
+  const ENDPOINT = 'localhost:3001'
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search)
+
+    socket = io(ENDPOINT)
+
+    setRoom(room)
+    setName(name)
+
+    socket.emit('join', { name, room }, () => {})
+
+    return () => {
+      socket.emit('disconnect')
+      socket.off()
+    }
+  }, [location.search, ENDPOINT])
+
   return <h1>Chat</h1>
 }
 
--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
+var cors = require('cors')
 
 const PORT = process.env.PORT || 5000
 
@@ -9,14 +10,23 @@ const router = require('./router')
 
 const app = express()
 const server = http.createServer(app)
-const io = socketio(server)
+const io = socketio(server, {
+  cors: {
+    origin: '*',
+  },
+})
+
+app.use(cors())
+app.use(router)
 
 io.on('connection', socket => {
   console.log('We have a new connection')
 
+  socket.on('join', ({ name, room }) => {
+    console.log(name, room)
+  })
+
   socket.on('disconnect', () => console.log('User had left'))
 })
-
-app.use(router)
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`))
