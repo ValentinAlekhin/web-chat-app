c15c54be87171a05a05d50c2305a681f452b2a98
Valentin Alekhin <headbangernetwork@gmail.com>
2021-02-07 13:48:04 +0300

    Finish Join

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import Join from './pages/Join.jsx'
-import Chat from './pages/Chat.jsx'
+import Join from './pages/Join/Join.jsx'
+import Chat from './pages/Chat/Chat.jsx'
 
 const App = () => (
   <Router>
