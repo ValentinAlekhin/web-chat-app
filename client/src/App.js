569425d36c546978c8883ee1a95a563bc3b1efed
Valentin Alekhin <headbangernetwork@gmail.com>
2021-02-07 13:19:36 +0300

    Init server

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
+import Join from './pages/Join.jsx'
+import Chat from './pages/Chat.jsx'
+
 const App = () => (
   <Router>
     <Route path="/" exact component={Join} />
     <Route path="/chat" exact component={Chat} />
   </Router>
 )
+
+export default App
